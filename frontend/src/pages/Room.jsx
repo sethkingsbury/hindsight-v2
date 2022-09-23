96e1a70b0269a1ef43c41212f65c6107486cfb17
Seth Kingsbury <swk22@uclive.ac.nz>
2022-09-23 17:44:54 +1200

    changed endpoint on frontend -trial

--- frontend/src/pages/Room.jsx
+++ frontend/src/pages/Room.jsx
@@ -3,12 +3,16 @@ import { useNavigate } from 'react-router-dom';
 import RoomHeader from '../components/RoomHeader';
 const { io } = require('socket.io-client');
 
+// const ENDPOINT = 'http://localhost:5000/';
+const ENDPOINT = 'https://https://hindsight.herokuapp.com/';
+
 function Room() {
 	const navigate = useNavigate();
 	const room = localStorage.getItem('room');
 	const name = localStorage.getItem('name');
 	const [users, setUsers] = useState([]);
-	const socket = io('http://localhost:5000/');
+
+	const socket = io(ENDPOINT);
 
 	const start = () => {
 		localStorage.setItem('answers', JSON.stringify([]));
